938bd770731ee8f4a424942b7473bcd8e1f7b12d
thomas.fruchart <thfruchart@stpbb.org>
2022-03-10 10:59:49 +0100

    Amélioration requête PDO

--- mot_du_titre/requete.php
+++ mot_du_titre/requete.php
@@ -1,20 +1,21 @@
-<?php 
+<?php
     require "../connexion.php";
 
-    $req = $bdd->prepare('SELECT * FROM `livre` WHERE `titre` LIKE "%'.$titre.'%";');
+    /*  REQUETE PRÉPARÉE  puis EXÉCUTÉE      */
+    $req = $bdd->prepare('SELECT * FROM `livre` WHERE `titre` LIKE :titre');
+    $titre = "%".$titre."%";
+    $req->bindParam(':titre', $titre, PDO::PARAM_STR);
     $req->execute();
-
-/*  REQUETE PRÉPARÉE  puis EXÉCUTÉE      */
-   /*  $req = $bdd->prepare('SELECT * FROM `livre` WHERE `titre` LIKE \'%?%\';');
-    $req->execute(array($titre)); */
     $reponse_mot_du_titre = $req->fetchAll(PDO::FETCH_ASSOC);
-/* Pour voir la reponse renvoyée par la requête : dé-commenter l'instruction suivante */
-    /* var_dump($reponse);  */
 
-/*  Pour voir comment parcourir chaque ligne de la variable $reponse, 
+
+/* Pour voir la reponse renvoyée par la requête : dé-commenter l'instruction suivante */
+    /* var_dump($reponse_mot_du_titre);  */
+
+/*  Pour voir comment parcourir chaque ligne de la variable $reponse,
     dé-commenter l'instruction suivante */
-    /*   foreach($reponse as $element) :
+    /*   foreach($reponse_mot_du_titre as $element) :
             var_dump($element);
             echo "<br>";
-    endforeach ; */ 
+    endforeach ; */
 ?>
